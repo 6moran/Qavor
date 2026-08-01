32676669510e55b6098b1f256c0c4a96112907a4
mrystudy-12 <3641525319@qq.com>
2026-08-01 20:51:43 +0800

    feat(sse): add default SSE config

---
+++ internal/sse/config.go
@@ -0,0 +1,11 @@
+package sse
+
+import "time"
+
+// DefaultConfig 返回默认 SSE 配置
+func DefaultConfig() *SSEConfig {
+	return &SSEConfig{
+		MaxStreamTime:     5 * time.Minute, // 单次流式最大5分钟
+		HeartbeatInterval: 15 * time.Second, // 心跳间隔：15秒
+	}
+}
